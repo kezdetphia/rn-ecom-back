d69cbe197aee0b454c7b41576ff21e1bf515cc68
kezdetphia <fehermark88@gmail.com>
2024-04-23 22:11:47 -0700

    fix paths of import

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const authController = require("../controllers/AuthController");
+const authController = require("../controllers/authControllers");
 
 router.post("/register", authController.createUser);
 router.post("/login", authController.loginUser);
