33c366aa83aca62697dc2b7ccf113dca086dba5a
kezdetphia <fehermark88@gmail.com>
2024-04-23 22:15:27 -0700

    fix paths of auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const authController = require("../controllers/authControllers");
+const authController = require("../controllers/authController");
 
 router.post("/register", authController.createUser);
 router.post("/login", authController.loginUser);
